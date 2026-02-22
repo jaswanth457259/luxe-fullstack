72e89f2b6429e46e3a44154834e5d4df77b5973e
Jaswanth Eedara <122589315+jaswanth457259@users.noreply.github.com>
2026-02-22 13:43:27 +0530

    Add JWT debug logs to diagnose 403 authentication issue

--- backend/src/main/java/com/luxe/ecommerce/security/JwtAuthFilter.java
+++ backend/src/main/java/com/luxe/ecommerce/security/JwtAuthFilter.java
@@ -26,6 +26,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("JWT FILTER EXECUTED");
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
